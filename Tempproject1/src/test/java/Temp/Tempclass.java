f5bdd60a50660b2b22f0e6d3c63af9ae875e20bf
503360078 <503360078@HC-CBKTDK3>
2025-03-29 08:02:08 +0530

    Push_3

---
+++ Tempproject1/src/test/java/Temp/Tempclass.java
@@ -0,0 +1,12 @@
+package Temp;
+
+public class Tempclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Temp class is created success for checking");
+
+	}
+
+}
